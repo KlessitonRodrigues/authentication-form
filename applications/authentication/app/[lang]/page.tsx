69a12256b9164adc30519d4e7f3a5a8706ffe7cd
klessiton <klessitom@gmail.com>
2026-04-02 11:46:34 -0300

    feat: add carousel login screen

--- applications/authentication/app/[lang]/page.tsx
+++ applications/authentication/app/[lang]/page.tsx
@@ -1,8 +1,12 @@
 import { generateStaticParams, useServerTranslations } from '@/lib/hooks/useServerTranslation';
 import AuthenticationView from '@/lib/views/authentication/AuthenticationView';
-import minimalBgImg from '@/public/images/minimal_bg.jpg';
+import beachBg1 from '@/public/images/beach_bg_01.jpg';
+import beachBg2 from '@/public/images/beach_bg_02.jpg';
+import beachBg3 from '@/public/images/beach_bg_03.jpg';
+import beachBg4 from '@/public/images/beach_bg_04.jpg';
+import beachBg5 from '@/public/images/beach_bg_05.jpg';
 import { NEXTJS } from '@packages/common-types';
-import { PageFull, SignInScreenV2, materialColors } from '@packages/daisy-ui-components';
+import { PageFull, SignInCarouselScreen, materialColors } from '@packages/daisy-ui-components';
 
 export { generateStaticParams };
 
@@ -11,16 +15,37 @@ export default async function RootPage(page: NEXTJS.PageProps) {
 
   return (
     <PageFull>
-      <SignInScreenV2
-        data={{
-          title: t('pages.signIn.title'),
-          description: t('pages.signIn.description'),
-          bgImageUrl: minimalBgImg.src,
-          bgBrightness: 0.75,
-        }}
+      <SignInCarouselScreen
+        data={[
+          {
+            title: t('pages.signIn.title'),
+            description: t('pages.signIn.description'),
+            imgUrl: beachBg1.src,
+          },
+          {
+            title: t('pages.signIn.title'),
+            description: t('pages.signIn.description'),
+            imgUrl: beachBg2.src,
+          },
+          {
+            title: t('pages.signIn.title'),
+            description: t('pages.signIn.description'),
+            imgUrl: beachBg3.src,
+          },
+          {
+            title: t('pages.signIn.title'),
+            description: t('pages.signIn.description'),
+            imgUrl: beachBg4.src,
+          },
+          {
+            title: t('pages.signIn.title'),
+            description: t('pages.signIn.description'),
+            imgUrl: beachBg5.src,
+          },
+        ]}
         bgColors={{
-          from: materialColors.deepPurple['100'],
-          to: materialColors.blue['50'],
+          from: materialColors.grey['100'],
+          to: materialColors.lightBlue['100'],
         }}
         loginForm={<AuthenticationView t={t} />}
       />
--- applications/authentication/app/page.tsx
+++ applications/authentication/app/page.tsx
@@ -1,12 +1,16 @@
 import { useServerTranslations } from '@/lib/hooks/useServerTranslation';
 import AuthenticationView from '@/lib/views/authentication/AuthenticationView';
-import minimalBgImg from '@/public/images/minimal_bg.jpg';
+import beachBg1 from '@/public/images/beach_bg_01.jpg';
+import beachBg2 from '@/public/images/beach_bg_02.jpg';
+import beachBg3 from '@/public/images/beach_bg_03.jpg';
+import beachBg4 from '@/public/images/beach_bg_04.jpg';
+import beachBg5 from '@/public/images/beach_bg_05.jpg';
 import { NEXTJS } from '@packages/common-types';
 import {
   LoadScreen,
   PageFull,
   RedirectPage,
-  SignInScreenV2,
+  SignInCarouselScreen,
   materialColors,
 } from '@packages/daisy-ui-components';
 import { Suspense } from 'react';
@@ -17,16 +21,37 @@ export default async function RootPage(page: NEXTJS.PageProps) {
   return (
     <PageFull>
       <Suspense fallback={<LoadScreen />}>
-        <SignInScreenV2
-          data={{
-            title: t('pages.signIn.title'),
-            description: t('pages.signIn.description'),
-            bgImageUrl: minimalBgImg.src,
-            bgBrightness: 0.75,
-          }}
+        <SignInCarouselScreen
+          data={[
+            {
+              title: t('pages.signIn.title'),
+              description: t('pages.signIn.description'),
+              imgUrl: beachBg1.src,
+            },
+            {
+              title: t('pages.signIn.title'),
+              description: t('pages.signIn.description'),
+              imgUrl: beachBg2.src,
+            },
+            {
+              title: t('pages.signIn.title'),
+              description: t('pages.signIn.description'),
+              imgUrl: beachBg3.src,
+            },
+            {
+              title: t('pages.signIn.title'),
+              description: t('pages.signIn.description'),
+              imgUrl: beachBg4.src,
+            },
+            {
+              title: t('pages.signIn.title'),
+              description: t('pages.signIn.description'),
+              imgUrl: beachBg5.src,
+            },
+          ]}
           bgColors={{
-            from: materialColors.deepPurple['100'],
-            to: materialColors.blue['50'],
+            from: materialColors.grey['100'],
+            to: materialColors.lightBlue['100'],
           }}
           loginForm={<AuthenticationView t={t} />}
         />
